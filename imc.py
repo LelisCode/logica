fc1b7ee69462b3974ff4cae87f90524540a866f2
Mileni Lelis Ramos Servulo <lelis.engineer@yahoo.com>
2026-03-24 17:17:33 -0300

    Add files via upload

---
+++ imc.py
@@ -0,0 +1,35 @@
+# Desenvolver um programa que leia o peso e a altura de uma pessoa e calcule seu imc utilizando a fórmula: 
+# imc = peso / altura ^ altura
+# Com o imc exiba para o usuário seu imc e a classificação:
+# IMC		Classificação
+# < 16		'Magreza grave'
+# 16 a < 17	'Magreza moderada'
+# 17 a < 18,5	'Magreza leve'
+# 18,5 a < 25	'Saudável'
+# 25 a < 30	'Sobrepeso'
+# 30 a < 35	'Obesidade Grau I'
+# 35 a < 40	'Obesidade Grau II (severa)'
+# ≥ 40		'Obesidade Grau III (mórbida)'
+
+p = float(input("Digite seu peso (kg): "))
+a = float(input("Digite sua altura (m): "))
+imc = p / (a ** 2)
+
+if imc < 16:
+    classificacao = "Magreza grave"
+elif 16 <= imc < 17:
+    classificacao = "Magreza moderada"
+elif 17 <= imc < 18.5:
+    classificacao = "Magreza leve"
+elif 18.5 <= imc < 25:
+    classificacao = "Saudável"
+elif 25 <= imc < 30:
+    classificacao = "Sobrepeso"
+elif 30 <= imc < 35:
+    classificacao = "Obesidade Grau I"
+elif 35 <= imc < 40:
+    classificacao = "Obesidade Grau II (severa)"
+else:
+    classificacao = "Obesidade Grau III (mórbida)"
+print(f"Seu IMC é: {imc:.2f} {classificacao}")
+
