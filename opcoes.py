8809155d3bbc7a112951c6e3a7ea7091ea4159f4
Mileni Lelis Ramos Servulo <lelis.engineer@yahoo.com>
2026-03-24 17:49:18 -0300

    Add files via upload

---
+++ opcoes.py
@@ -0,0 +1,19 @@
+print("Opção 1")
+print("Opção 2")
+print("Opção 3")
+print("Sair")
+
+o3 = input("Escolha uma opção: ")
+
+if o3 == "Opção 1":
+    print("Opção 1 selecionada")
+elif o3 == "Opção 2":
+    print("Opção 2 selecionada")    
+elif o3 == "Opção 3":
+    print("Opção 3 selecionada")
+elif o3 == "Sair":
+    print("Saindo do programa...")  
+else:
+    print("Está opção é inexistente.")
+
+    print(f"Sua escolha foi: {o3}")
