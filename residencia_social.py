c4c584475288a113e70d662745561f8a7fc6d2a3
Mileni Lelis Ramos Servulo <lelis.engineer@yahoo.com>
2026-03-26 10:38:24 -0300

    Refactor water consumption calculation logic

--- residencia_social.py
+++ residencia_social.py
@@ -6,8 +6,12 @@
 # Se o consumo for menor ou igual a 50m3, então R$ 6,62 por m3
 # Se o consumo for acima dos 50m3, então R$ 7,31 por m3
 
+
+#Entrada
 consumos = float(input("Digite o consumo de água da residência social em m3: "))
 
+#Processamento
+#Valores que utilixam o pow para estarem elevados ao número solicitado e comparadores para definir sua conta de acordo com o input do usuário
 if consumos <= pow(10, 3):
     valor = consumos * 7.59
 elif consumos <= pow(20, 3): 
@@ -21,6 +25,6 @@ elif consumos <= pow(50, 3):
 else:
     valor = consumos * 7.31
 
-
+#Saída
 print(f"O consumo de água de sua residência social irá custar R$ {valor:.2f}")
 
